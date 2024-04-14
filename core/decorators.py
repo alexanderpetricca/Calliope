e021be68316ba4d41ce4312216a39df8f41f8fbd
Alexander Petricca <petchalxande@gmail.com>
2024-04-14 08:57:58 +0100

    Collectstatic

--- core/decorators.py
+++ core/decorators.py
@@ -6,9 +6,13 @@ from django.http import HttpResponseForbidden
 def require_htmx(view_func):
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
-        """Validates that a view is being called from a HTMX request."""
+        """
+        Validates that a view is being called from a HTMX request.
+        """
+
         if request.headers.get('HX-Request', '') == 'true':
             return view_func(request, *args, **kwargs)
         else:
             return HttpResponseForbidden("Access Denied")
+    
     return _wrapped_view
