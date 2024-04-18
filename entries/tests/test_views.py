3c235d7852860a87b496d45d04a71e08503abcee
Alexander Petricca <petchalxande@gmail.com>
2024-04-18 15:44:10 +0100

    Added new test for HTMX entry view.

--- entries/tests/test_views.py
+++ entries/tests/test_views.py
@@ -55,8 +55,56 @@ class EntryViewTests(TestCase):
         self.assertContains(response, 'Calliope | Home')
 
 
-    # HTMX List View
+    # HTMX Entry List
+
+    def testHTMXEntryListViewLoggedOut(self):
+        """
+        Tests if user is redirected to login when signed out, whilst trying to access the entry list HTMX endpoint.
+        """
+        
+        self.client.logout()
+        
+        response = self.client.get(reverse('entries_entry_list'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, f"{reverse('login')}?next=/entry-list/")
+        
+        response = self.client.get(f"{reverse('login')}?next=/entry-list/")
+        self.assertTemplateUsed(response, 'registration/login.html')
+        self.assertContains(response, 'Calliope | Login')
+
+
+    def testHTMXEntryListViewLoggedInNonHTMX(self):
+        """
+        Tests a 403 is returned when trying to access the HTMX entry list endpoint, when logged in.
+        """
+
+        self.client.login(email="testuser@email.com", password="testpass123")  
+        
+        response = self.client.get(reverse('entries_entry_list'))
+        self.assertEqual(response.status_code, 403)
+
+
+    def testEntryListViewLoggedIn(self):
+        """
+        Tests the entry list view page is returned when user is logged in, with HTMX.
+        """
+
+        self.client.login(email="testuser@email.com", password="testpass123")
+        
+        headers = {
+            'HX-Request': 'true',
+            'Content-Type': 'text/html',
+        }
+
+        response = self.client.get(
+            reverse('entries_entry_list'), 
+            **{'HTTP_' + k.replace('-', '_').upper(): v for k, v in headers.items()}
+        )
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'entries/entry-list.html')
+
+
     # HTMX Search View
-    # HTMX Create View
-    # HTMX Update View
+    # HTMX Entry View
     # HTMX Delete View
